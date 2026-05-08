6532d8f08e18b78d16b08182dddb85adc93383c0
thiaguinhodopneu <thirodriguesnogueira07@gmail.com>
2026-05-07 21:02:15 -0300

    Primeira versão do projeto

--- services/ia_services.py
+++ services/ia_services.py
@@ -11,4 +11,21 @@ class IAService:
         
         self.cliente = Groq()
 
-    def enviar_mensagem(self, historico: list, system_prompt)
+    def enviar_mensagem(self, historico: list, system_prompt) str
+        
+        try:
+            
+            mensagens = [{"role": "system", "content": system_prompt}] + historico
+
+            resposta = self.cliente.chat.completions.create(
+                model=self.MODELO,
+                max_tokens=self.MAX_TOKENS
+                messages=mensagens,
+            )
+
+            return resposta.choices[0].message.cotent
+        
+        except Exception as e:
+            mensagem = str(e)
+            if"401" in mensagem or "invalid_api_key" in mensagem.lower():
+                raise Exception(" Erro de autenticação: verifique sua GROQ_API")
